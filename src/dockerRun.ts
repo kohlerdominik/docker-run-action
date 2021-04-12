846ad94c45c2361f3eddb149e42c94a6e16b7c48
Dominik Kohler <dominik.kohler@lextira.ch>
2021-04-12 21:41:30 +0200

    Initial Commit

---
+++ src/dockerRun.ts
@@ -0,0 +1,39 @@
+import * as core from '@actions/core'
+import * as exec from '@actions/exec'
+import * as fs from 'fs'
+import {v4 as uuidv4} from 'uuid'
+import * as input from './input'
+
+export const COMMAND_FILE_NAME = `${uuidv4()}.sh`
+export const LOCAL_COMMAND_PATH = `/home/runner/work/_temp/${COMMAND_FILE_NAME}`
+export const CONTAINER_COMMAND_PATH = `/tmp/${COMMAND_FILE_NAME}`
+
+export async function runContainer(): Promise<void> {
+  fs.writeFileSync(LOCAL_COMMAND_PATH, input.get('run'), {mode: 0o755})
+  core.info(`
+These instructions are passed to the container:
+----- START OF FILE -----
+${fs.readFileSync(LOCAL_COMMAND_PATH).toString()}
+----- END OF FILE -----`)
+
+  await exec.exec('docker', [
+    `run`,
+    // default network
+    ...(input.has('default_network')
+      ? [`--network=${input.get('default_network')}`]
+      : []),
+    // workdir
+    ...(input.has('workdir') ? [`--workdir=${input.get('workdir')}`] : []),
+    // environment options
+    ...input.getEnvironment(),
+    // volume options
+    `--volume=${LOCAL_COMMAND_PATH}:${CONTAINER_COMMAND_PATH}`,
+    ...input.getVolumes(),
+    // other options
+    ...input.getSplittet('options'),
+    input.get('image'),
+    input.get('shell'),
+    '-e',
+    CONTAINER_COMMAND_PATH
+  ])
+}
---
+++ src/main.ts
@@ -0,0 +1,17 @@
+import * as os from 'os'
+import * as core from '@actions/core'
+import * as dockerRun from './dockerRun'
+
+async function run(): Promise<void> {
+  try {
+    await dockerRun.runContainer()
+
+    if (os.platform() !== 'linux') {
+      throw new Error('This actions works only on linux platform.')
+    }
+  } catch (error) {
+    core.setFailed(error.message)
+  }
+}
+
+run()

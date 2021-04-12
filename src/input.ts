1d9677a1f862c372b4e519959b9318cb60fb577c
Dominik Kohler <dominik.kohler@lextira.ch>
2021-04-12 21:35:30 +0200

    Initial Commit

---
+++ src/input.ts
@@ -0,0 +1,29 @@
+import * as core from '@actions/core'
+
+const required = ['image', 'run']
+
+export function has(name: string): boolean {
+  return !!get(name)
+}
+
+export function get(name: string): string {
+  return core.getInput(name, {
+    required: required.includes(name)
+  })
+}
+
+export function getSplittet(name: string): string[] {
+  return get(name)
+    .split(/[\s]+/)
+    .filter((instruction: string) => !!instruction)
+}
+
+export function getEnvironment(): string[] {
+  return getSplittet('environment').map(
+    (variable: string) => `--env=${variable}`
+  )
+}
+
+export function getVolumes(): string[] {
+  return getSplittet('volumes').map((volume: string) => `--volume=${volume}`)
+}
---
+++ tests/test.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+pass() {
+  echo "test passed"
+  exit 0
+}
+
+fail() {
+  echo "test failed"
+  exit 1
+}
+
+case $1 in
+  "equal")
+    [ "${2}" == "${3}" ] && pass || fail
+    ;;
+  "empty")
+    [ -z "${2}" ] && pass || fail
+    ;;
+  "not-empty")
+    [ ! -z "$2" ] && pass || fail
+    ;;
+  *)
+    echo "unkown test"
+    exit 1
+    ;;
+esac

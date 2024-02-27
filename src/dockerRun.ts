e426b9b8a9c570ce0c921b827831e0eca1faf5a3
Dominik Kohler <dominik.kohler@komail.ch>
2024-02-27 14:36:18 +0100

    Create files sync

--- src/dockerRun.ts
+++ src/dockerRun.ts
@@ -18,7 +18,7 @@ const command = fileMap.pushRunnerPath(
 )
 
 export async function runContainer(): Promise<void> {
-  fileMap.items.forEach((items: Mapping) => fs.appendFile(items.runner.path, '', ()=>{},))
+  fileMap.items.forEach((items: Mapping) => fs.appendFileSync(items.runner.path, ''))
   fs.writeFileSync(command!.runner.path, input.get('run'), {mode: 0o755})
   core.info(`
 Wrote instruction file to "${command!.runner.path}"
